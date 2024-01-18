333b183bca8772693cd88a8e4ca6ae66b4b13890
wanjiaqi <1420537511@qq.com>
2024-01-18 15:29:44 +0800

    语音接口和ai接口结合成功

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 const btn = document.querySelector('button');
 const info = document.querySelector('#info');
-btn.addEventListener('click',async function(e){
-   const a = await axios.get("https://api.lolimi.cn/API/AI/wr.php",{params:{msg: test.value}})
-   let num = Math.floor(Math.random()*a.data.data.output.length)
-   console.log(a.data.data.output);
-   info.insertAdjacentHTML("beforeend",`<li>${a.data.data.output.join(",")}</li>`)
+const audio = document.querySelector('audio');
+btn.addEventListener('click', async function (e) {
+   try {
+      const a = await axios.get("https://api.lolimi.cn/API/AI/bj.php",{params:{msg:test.value}})
+      const b= await axios.get("https://api.lolimi.cn/API/yyhc/y.php",{params:{msg:a.data.data.output,speaker:"派蒙",noisew:"0.5",sdp:"2",Length:"1",noise:"0.6",type:"1"}})
+      audio.src = b.data.music
+      audio.play()
+      info.insertAdjacentHTML("beforeend",`<li>${a.data.data.output}</li>`)
+      console.log(b);
+   } catch (error) {
+      console.log(error);
+   }
+   
 })
